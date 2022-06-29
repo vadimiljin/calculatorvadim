9f9ab395406f1b91920d1431e00b5308e95bc3ff
Vadim Iljin <vadim.iljin@gmail.com>
2022-06-29 07:05:49 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Calculator
+This calculator package can perform addition, subtraction, multiplication, and division, as well as taking a (n) root of a number and also resetting its memory. 
+
+For each operation, it takes only one input number and combines it with the current value in its own memory, which can be reset to zero.
+
+The technology stack for this project includes `Python` for building the package, `Docker` for containerizing the package, and `Pytest` for testing the package.
+
+---
+## Features
+
+- Addition: The input value is added to the memory value.
+- Subtraction: The input value is subtracted from the memory value.
+- Multiplication: The input value is multiplied by the memory value.
+- Division: The input value is divided by the memory value.
+- Root: The input value root of the memory value is returned.
+- Reset: The memory value is reset to zero.
+
+---
+## Installation
+
+You can run any of the commands below to install the package.
+
+- To install the package from PyPI
+
+```shell
+$ pip install calculatorvadim
+```
+
+- To install from github
+
+```shell
+$ pip install git+https://github.com/vadimiljin/calculatorvadim.git
+```
+
+- To get the calculator image from dockerhub 
+
+```shell
+$ docker pull iljinvadim/calculatorvadim
+```
+
+---
+## Usage
+
+Usage examples for each method are given below.
+
+```python
+>>> from calculatorvadim import Calculator
+>>> calculator = Calculator()
+>>> calculator.add(4)
+4
+>>> calculator.subtract(2)
+2
+>>> calculator.multiply(12)
+24
+>>> calculator.divide(3)
+8.0
+>>> calculator.root(3)
+2.0
+>>> calculator.reset()
+0
+```
+
+---
+## Testing
+
+```shell
+$ pytest
+```
+
+---
+## License 
+
+This project is licensed under the terms of the MIT license
+
+---
+## Author
+
+Vadim Iljin
---
+++ calculatorvadim.py
@@ -0,0 +1,87 @@
+"""Let's calculate"""
+
+__version__ = "0.0.1"
+
+class Calculator:
+    """
+    This class represents a calculator.
+
+    ...
+
+    Attributes
+    ----------
+    memory_value : float
+        value in calculator memory
+    """
+
+    def __init__(self, memory_value: float = 0):
+        self._memory_value = self._validate_memory_value(memory_value)
+        """
+        This method constructs the necessary attributes for the calculator object.
+
+        Parameters
+        ----------
+        memory_value : float
+            value of the memory
+        """
+
+    def add(self, input_value: float) -> float:
+        """This method accepts a value and increments it by the value currently stored in memory, returning the new value."""
+        self._validate_input(input_value)
+        self._memory_value += input_value
+        return self._memory_value
+
+    def subtract(self, input_value: float) -> float:
+        """This method accepts a value and subtracts it from the value stored in memory, returning the difference."""
+        self._validate_input(input_value)
+        self._memory_value -= input_value
+        return self._memory_value
+
+    def multiply(self, input_value: float) -> float:
+        """This method takes in a value and multiplies it with the value in memory, returning the multiplication."""
+        self._validate_input(input_value)
+        self._memory_value *= input_value
+        return self._memory_value
+
+    def divide(self, input_value: float) -> float:
+        """This method takes in a value and divides it by the value in memory, returning the division."""
+        self._validate_input(input_value)
+        self._division_by_zero(input_value)
+        self._memory_value /= input_value
+        return self._memory_value
+
+    def root(self, input_value: float) -> float:
+        """This method takes in a value n and returns the nth root of the value stored in memory."""
+        self._validate_input(input_value)
+        self._division_by_zero(input_value)
+        self._complex_number_for_root(input_value)
+        self._memory_value = self._memory_value ** (1.0 / input_value)
+        return self._memory_value
+
+
+    def reset(self) -> float:
+        """This method will set the value stored in memory to zero."""
+        self._memory_value = 0
+        return self._memory_value
+
+    def _validate_memory_value(self, new_value: float) -> float:
+        if not isinstance(new_value, (int, float)):
+            raise ValueError("Please enter a valid number for the calculator's memory value!")
+        self._memory_value = new_value
+        return self._memory_value
+
+    def _validate_input(self, input_value: float) -> float:
+        if isinstance(input_value, (int, float)):
+            return input_value
+        raise TypeError("Please enter a valid number!")
+
+    def _division_by_zero(self, input_value: float) -> float:
+        if input_value == 0:
+            raise ZeroDivisionError("Dividing by zero is not a valid operation!")
+        return input_value
+
+
+    def _complex_number_for_root(self, input_value: float) -> float:
+        if self._memory_value < 0:
+            raise ValueError("The root of a complex or negative number is not allowed.")
+        return input_value
